250227ed5573ba030b514ab93ad85ed56fe6f893
Anthony Bernardo Kamers <anthony.kamers@ufsc.br>
2023-10-04 21:55:02 -0300

    First commit 	* The initial files for the Flask server are done 	* An Index with the main questions about the project 	* Main style in CSS 	* Main structure of files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('pages/404.jinja'), 404
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('pages/index.jinja', name="Anthony Bernardo Kamers")
+
+
+if __name__ == '__main__':
+    app.run()
